7086f0a3ce24006f1aae5fad593d054e4660b87f
coco-29 <cocodamva@gmail.com>
2018-01-10 17:02:51 -0600

    cambios

--- src/main/java/com/mx/mwisp/mwsipfinal/configuration/UploadConfig.java
+++ src/main/java/com/mx/mwisp/mwsipfinal/configuration/UploadConfig.java
@@ -1,5 +1,8 @@
 package com.mx.mwisp.mwsipfinal.configuration;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.apache.commons.logging.Log;
@@ -16,10 +19,20 @@ public class UploadConfig extends WebMvcConfigurerAdapter{
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// TODO Auto-generated method stub
 		super.addResourceHandlers(registry);
+		Path rootBase=Paths.get("uploads").toAbsolutePath();
 		String resourcePath=Paths.get("uploads").toAbsolutePath().toUri().toString();	
 				//String resourcePath=Paths.get("/Users/maya/Desktop/uploads").toAbsolutePath().toUri().toString();
 				//LOG.info(resourcePath);
+		if(!Files.exists(rootBase)) {
+			try {
+				Files.createDirectories(rootBase);
 				LOG.info("resourcePath========================================================="+resourcePath);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+				
 		registry.addResourceHandler("/uploads/**")
 		.addResourceLocations(resourcePath);
 	}
