0fd66b8db056ca906a9bcfef9f2fb7363f993f2d
maya <maya@192.168.0.111>
2018-01-21 17:00:41 -0600

    Cambios vista Detalles de compras... y del Wizard

--- src/main/resources/static/ecommerce/js/wizard.js
+++ src/main/resources/static/ecommerce/js/wizard.js
@@ -11,7 +11,6 @@ $(document).ready(function () {
 			$('#r13').on('click', function(){
 		  $(this).parent().find('a').trigger('click')
 		})
-	
     var navListItems = $('div.setup-panel div a'),
             allWells = $('.setup-content'),
             allNextBtn = $('.nextBtn');
@@ -36,7 +35,7 @@ $(document).ready(function () {
         var curStep = $(this).closest(".setup-content"),
             curStepBtn = curStep.attr("id"),
             nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-            curInputs = curStep.find("input[type='text'],input[type='url']"),
+            curInputs = curStep.find("input[type='text'],input[type='url'],input[type='email'],input[type='number']"),
             isValid = true;
 
         $(".form-group").removeClass("has-error");
@@ -52,4 +51,31 @@ $(document).ready(function () {
     });
 
     $('div.setup-panel div a.btn-primary').trigger('click');
+    
+    
+
+        OpenPay.setId('mexcviwsqt2snzeylcy5');
+        OpenPay.setApiKey('pk_41f1c1b5d9e3450a85f4e5e882c13ca7');
+        OpenPay.setSandboxMode(true);
+        //Se genera el id de dispositivo
+        var deviceSessionId = OpenPay.deviceData.setup("payment-form", "deviceIdHiddenFieldName");
+        
+        $('#pay-button').on('click', function(event) {
+         event.preventDefault();
+            $("#pay-button").prop( "disabled", true);
+            OpenPay.token.extractFormAndCreate('payment-form', sucess_callbak, error_callbak);                
+        });
+ 
+        var sucess_callbak = function(response) {
+          var token_id = response.data.id;
+        
+          $('#token_id').val(token_id);
+          $('#payment-form').submit();
+        };
+
+        var error_callbak = function(response) {
+            var desc = response.data.description != undefined ? response.data.description : response.message;
+            alert("FAVOR DE VERIFICAR SUS DATOS");
+            $("#pay-button").prop("disabled", false);
+        };
 });
